59b831586b7622daf88650a34cb47dfbd4bb5a39
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-18 17:00:51 -0500

    [add] - Watch App 🤓

---
+++ stepCounterWatch Watch App/stepCounterWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepCounterWatchApp.swift
+//  stepCounterWatch Watch App
+//
+//  Created by Pierre Juarez U. on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct stepCounterWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
