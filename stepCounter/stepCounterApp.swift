d79bdae9af163f93fbb233e9ba25677836303883
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-18 10:42:14 -0500

    [add] - Inital commit 🚀

---
+++ stepCounter/stepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepCounterApp.swift
+//  stepCounter
+//
+//  Created by Pierre Juarez U. on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct stepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
