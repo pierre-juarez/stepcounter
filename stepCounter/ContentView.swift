66a79338ed6db52df434ce8e093bd898dac3e877
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-19 00:33:55 -0500

    [add] - Reuse package in view and model 👀

--- stepCounter/ContentView.swift
+++ stepCounter/ContentView.swift
@@ -10,28 +10,8 @@ import StepCounterModule
 
 struct ContentView: View {
     
-    @StateObject var viewModel = ViewModel()
-    
     var body: some View {
-        VStack {
-            Image(systemName: "figure.walk")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(.green)
-                .frame(width: 60, height: 60)
-            Text("Steps Today")
-                .font(.system(size: 20, weight: .bold, design: .rounded))
-            Text(viewModel.allMySteps)
-                .font(.system(size: 40, weight: .bold, design: .rounded))
-        }
-        .padding(12)
-        .overlay(
-            RoundedRectangle(cornerRadius: 8)
-                .stroke(style: StrokeStyle(lineWidth: 3, dash: [5]))
-        )
-        .task {
-            viewModel.requestAccessToHealthData()
-        }
+        StepsView()
     }
 }
 
--- stepCounterWatch Watch App/ContentView.swift
+++ stepCounterWatch Watch App/ContentView.swift
@@ -6,19 +6,14 @@
 //
 
 import SwiftUI
+import StepCounterModule
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+       StepsView()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
